9baebab8182c3bb97c5a66efe491067e4d1de902
s2alua <s2alua.fu@gmail.com>
2022-12-29 13:58:56 +0000

    init

---
+++ flashcard/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Flash card table
+class Flash(models.Model):
+    key=models.CharField(max_length=30)
+    answer=models.TextField()
+    tags=models.ManyToManyField('Tag')
+    created_at=models.DateField(auto_now_add=True)
+    count=models.IntegerField()
+
+    def __str__(self):
+        return self.key
+
+# Tags table
+class Tag(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
